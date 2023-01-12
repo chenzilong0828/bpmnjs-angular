a30483ae86471e78cb316e237fa5de39ced3b17b
chenzl <779885796@qq.com>
2023-01-13 00:26:48 +0800

    增加bpmn.js依赖

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DiagramComponent } from './diagram/diagram.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DiagramComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/diagram/diagram.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diagram',
+  templateUrl: './diagram.component.html',
+  styleUrls: ['./diagram.component.scss']
+})
+
+export class DiagramComponent {
+  bpmnModeler:any;
+
+  ngOnInit(): void {
+    
+  }
+}
